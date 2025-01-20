1c2661a48faca33b03f2955d33342998c7346ace
Aliaksei Kirychek <kirichek.miryk@gmail.com>
2025-01-20 17:00:24 +0300

    Add pydantic schema hotels

--- hotels.py
+++ hotels.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Query, Body
+from schemas.hotels import Hotel, HotelPATCH
 
 
 router = APIRouter(prefix="/hotels", tags=["Hotels"])
@@ -7,6 +8,12 @@ router = APIRouter(prefix="/hotels", tags=["Hotels"])
 hotels = [
     {"id": 1, "title": "Sochi", "name": "sochi"},
     {"id": 2, "title": "Дубай", "name": "дубай"},
+    {"id": 3, "title": "Sochi1", "name": "sochi1"},
+    {"id": 4, "title": "Дубай2", "name": "дубай2"},
+    {"id": 5, "title": "Sochi3", "name": "sochi3"},
+    {"id": 6, "title": "Дубай4", "name": "дубай4"},
+    {"id": 7, "title": "Sochi5", "name": "sochi5"},
+    {"id": 8, "title": "Дубай6", "name": "дубай6"},
 ]
 
 
@@ -26,10 +33,22 @@ def get_hotels(
 
 
 @router.post("")
-def create_hotel(title: str = Body(embed=True)):
+def create_hotel(
+        hotel_data: Hotel = Body(openapi_examples={
+            "1": {"summary": "Dubai", "value": {
+                "title": "Dubai",
+                "name": "dubai"
+            }},
+            "2": {"summary": "Hrodno", "value": {
+                "title": "Hrodno",
+                "name": "hrodno"
+            }}
+        })
+):
     new_hotel = {
         "id": hotels[-1]["id"] + 1 if hotels else 1,
-        "title": title
+        "title": hotel_data.title,
+        "name": hotel_data.name
     }
     hotels.append(new_hotel)
     return {"status": "OK"}
@@ -38,32 +57,30 @@ def create_hotel(title: str = Body(embed=True)):
 @router.put("/{hotel_id}")
 def replace_hotels(
         hotel_id: int,
-        title: str = Body(),
-        name: str = Body()
+        hotel_data: Hotel
 ):
     global hotels
     for hotel in hotels:
         if hotel["id"] != hotel_id:
             continue
-        hotel["title"] = title
-        hotel["name"] = name
+        hotel["title"] = hotel_data.title
+        hotel["name"] = hotel_data.name
     return {"status": "OK"}
 
 
 @router.patch("/{hotel_id}")
 def update_hotels(
         hotel_id: int,
-        title: str | None = Body(None),
-        name: str | None = Body(None)
+        hotel_data: HotelPATCH
 ):
     global hotels
     for hotel in hotels:
         if hotel["id"] != hotel_id:
             continue
-        if title:
-            hotel["title"] = title
-        if name:
-            hotel["name"] = name
+        if hotel_data.title:
+            hotel["title"] = hotel_data.title
+        if hotel_data.name:
+            hotel["name"] = hotel_data.name
     return {"status": "OK"}
 
 
