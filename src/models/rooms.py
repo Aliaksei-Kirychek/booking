27d3c7b46e05bdeb0adcce3dbdf886878c10c1a2
Aliaksei Kirychek <kirichek.miryk@gmail.com>
2025-01-21 13:01:48 +0300

    Add model rooms

---
+++ src/models/rooms.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String, ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base
+
+
+class RoomsORM(Base):
+    __tablename__ = "rooms"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotels.id"))
+    title: Mapped[str] = mapped_column(String(100))
+    description: Mapped[str | None] = mapped_column(String())
+    price: Mapped[int] = mapped_column(Integer())
+    quantity: Mapped[int] = mapped_column(Integer())
