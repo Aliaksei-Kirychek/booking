4426543c5858468acad941deef93e2e5098e232b
Aliaksei Kirychek <kirichek.miryk@gmail.com>
2025-01-22 12:31:15 +0300

    Add endpoint get/hotel_id

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -25,6 +25,15 @@ async def get_hotels(
         )
 
 
+@router.get("/{hotel_id}")
+async def get_hotel_by_id(hotel_id: int):
+    async with async_session_maker() as session:
+        hotel = await HotelsRepository(session).get_one_or_none(id=hotel_id)
+        if hotel is None:
+            raise HTTPException(status_code=404, detail="Hotel not found")
+        return {"status": "OK", "data": hotel}
+
+
 @router.post("")
 async def create_hotel(
         hotel_data: Hotel = Body(openapi_examples={
